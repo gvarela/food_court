9d0b33e9e0beffbc6dfc0040bb01d933e56d4735
Gabe Varela <gvarela@gmail.com>
2009-11-28 00:07:54 -0700

    get the simple configuration method specs running

--- lib/food_court/recipes.rb
+++ lib/food_court/recipes.rb
@@ -8,14 +8,14 @@ make_notify_task = lambda do
 
   namespace :food_court do
 
-    # run chauffeur chef-solo on the remote server
-    desc "setup"
-    task :setup, :only => {:chauffeur => true } do
+    # run food_court chef-solo on the remote server
+    desc "setup chef-solo on the remote server"
+    task :setup, :only => {:food_court => true } do
       
     end
 
-    desc "deploy your chauffeur chef-solo cookbooks"
-    task :deploy, :only => {:chauffeur => true } do
+    desc "deploy your food_court chef-solo cookbooks"
+    task :deploy, :only => {:food_court => true } do
 
     end
   end
@@ -23,7 +23,7 @@ make_notify_task = lambda do
 end
 require 'capistrano/version'
 if Capistrano::Version::MAJOR < 2
-  STDERR.puts "Unable to load #{__FILE__}\nChauffeur Capistrano hooks require at least version 2.0.0"
+  STDERR.puts "Unable to load #{__FILE__}\nFood Court Capistrano hooks require at least version 2.0.0"
 else
   instance = Capistrano::Configuration.instance
   if instance
--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -2,7 +2,33 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "FoodCourt::Command" do
   before do
-    @order = File.open( File.join( FoodCourt::Command::TEMPLATE_PATH, '/slicehost', 'order.rb'), 'w' ) { |f| f.write '{}' }
+    @order = File.open( File.join( FoodCourt::Command::TEMPLATE_PATH, '/slicehost', 'order.rb'), 'w' ) do |f|
+      f.write <<-EOH
+{
+  :file_cache_path => "/var/chef",
+  :cookbooks => [ '' ],
+
+  :dna => {
+    :apps_dir => "/data/my_app",
+    :applications => [
+        {
+            :name => "my_app",
+            :server_name => "my_app.com",
+            :server_aliases => ["www.my_app.com"],
+            }
+    ],
+    :recipes => [
+        "git",
+        "mysql",
+        "passenger",
+        "nginx",
+        "applications"
+    ]
+}
+}
+      EOH
+
+    end
     @default_recipe = File.open( File.join( FoodCourt::Command::TEMPLATE_PATH, '/slicehost/site-cookbooks/applications/recipes', 'default.rb'), 'w' ) { |f| f.write 'test' }
   end
 
@@ -28,7 +54,34 @@ describe "FoodCourt::Command" do
     it "should copy over order.rb to chef/order.rb" do
       order_path = File.join(@path, 'config/chef/order.rb')
       File.file?(order_path).should be_true
-      File.read(order_path).should == '{}'
+      File.read(order_path).should_not be_empty
+    end
+  end
+
+  context "#configure" do
+    before do
+      FoodCourt::Command.new('setup', 'slicehost')
+      @command = FoodCourt::Command.new('configure')
+    end
+
+    it "should eval file into @config as a hash" do
+      @command.config.should be_a Hash
+    end
+
+    it "should have :dna in the config hash" do
+      @command.config[:dna].should be_a Hash
+    end
+  end
+
+  context "#prepare" do
+    before do
+      FoodCourt::Command.new('setup', 'slicehost')
+      @command = FoodCourt::Command.new('configure')
+      @command.package
+    end
+
+    it "should compile dna.json" do
+      pending
     end
   end
 end
